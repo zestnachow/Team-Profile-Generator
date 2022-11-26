0402e3c86f1ce4f89ba285f5e47f80d3c5c396a0
Zach Weston <zachpweston@gmail.com>
2022-11-25 21:15:30 -0700

    created employee cards

--- src/template.js
+++ src/template.js
@@ -1,17 +1,55 @@
 // Template literal for manager card
 const createManagerCard = (manager) => {
     return `
-    `
+<div class="col-4 mt-6">
+    <div class="card h-100">
+        <div class="card-header">
+            <h3>${manager.name}</h3>
+            <h4>Manager</h4><i class="material-icons">groups</i>
+        </div>
+        <div class="card-body">
+            <p class="id">ID: ${manager.id}</p>
+            <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
+            <p class="office-number">Office Number: ${manager.officeNumber}</p>
+        </div>
+    </div>
+</div>`;
 }
 
 // Template literal for engineer card
 const createEngineerCard = (engineer) => {
-    return ``
+    return `
+<div class="col-4 mt-6">
+    <div class="card h-100">
+        <div class="card-header">
+            <h3>${engineer.name}</h3>
+            <h4>Engineer</h4><i class="material-icons">engineering</i>
+        </div>
+        <div class="card-body">
+            <p class="id">ID: ${engineer.id}</p>
+            <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+            <p class="github">Github: ${engineer.github}</p>
+        </div>
+    </div>
+</div>`;
 }
 
 // Template literal for intern card
 const createInternCard = (intern) => {
-    return ``
+    return `
+<div class="col-4 mt-6">
+    <div class="card h-100">
+        <div class="card-header">
+            <h3>${intern.name}</h3>
+            <h4>Intern</h4><i class="material-icons">school</i>
+        </div>
+        <div class="card-body">
+            <p class="id">ID: ${intern.id}</p>
+            <p class="email">Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+            <p class="school">School: ${intern.school}</p>
+        </div>
+    </div>
+</div>`;
 }
 
 // Function to generate requested cards based on user input
